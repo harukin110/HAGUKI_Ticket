a681e6e643ea376140d3f9a1ef73f911d9fe9574
polunga10 <ishigami.yuya.ov@mynavi.jp>
2024-06-19 05:26:15 +0000

    新規会員登録機能途中まで

--- flask_app/views/user/user_signup.py
+++ flask_app/views/user/user_signup.py
@@ -13,13 +13,33 @@ errorMessages = ErrorMessages()
 # 新規会員登録
 @app.route("/user_signup", methods=["GET", "POST"])
 def user_signup():
+    if request.method == 'POST':
+        redirect(url_for('user_signup_check'))
     return render_template("/user/signup/user_signup.html")
 
 
 # 新規会員登録確認画面
 @app.route("/user_signup_check", methods=["GET", "POST"])
 def user_signup_check():
-    return render_template("/user/signup/user_signup_check.html")
+
+    customer_id = request.form.get('customer_id')
+    customer_account = request.form.get('customer_account')
+    customer_password = request.form.get('customer_password')
+    customer_name = request.form.get('customer_name')
+    customer_zipcode = request.form.get('customer_zipcode')
+    customer_address = request.form.get('customer_address')
+    customer_phone = request.form.get('customer_phone')
+    customer_birth = request.form.get('customer_birth')
+
+    return render_template("/user/signup/user_signup_check.html",
+                           customer_id = customer_id,
+                           customer_account = customer_account,
+                           customer_password = customer_password,
+                           customer_name = customer_name,
+                           customer_zipcode = customer_zipcode,
+                           customer_address = customer_address,
+                           customer_phone = customer_phone,
+                           customer_birth = customer_birth)
 
 # 新規会員登録完了
 @app.route("/user_signup_comp", methods=["GET", "POST"])
