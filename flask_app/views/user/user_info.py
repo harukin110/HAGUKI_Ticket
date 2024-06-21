28f002f9fbbbc713048f0d6c15925b018b9de04e
SatoTatsuhiko1 <sato.tatsuhiko.tc@mynavi.jp>
2024-06-21 02:16:02 +0000

    user_info

--- flask_app/views/user/user_info.py
+++ flask_app/views/user/user_info.py
@@ -2,7 +2,9 @@ from flask import render_template, flash, request, redirect, session,url_for
 from flask_app.__init__ import app
 from flask_app.messages import ErrorMessages, InfoMessages
 from flask_app.models.functions.staff import read_staff_staff_account
+from flask_app.models.functions.customer import read_customer_customer_account
 from flask_app.views.staff.common.staff_common import is_staff_login
+
 
 # インフォメーションメッセージクラスのインスタンス作成
 infoMessages = InfoMessages()
@@ -11,27 +13,12 @@ errorMessages = ErrorMessages()
 
 #会員情報
 @app.route("/user_info", methods=["GET", "POST"])
-@is_staff_login
-def user_info(mode):
-    customer_id = request.form['customer_id']
-    customer_account = request.form['customer_account']
-    customer_password = request.form['customer_password']
-    customer_name = request.form['customer_name']
-    customer_zipcode = request.form['customer_zipcode']
-    customer_address = request.form['customer_address']
-    customer_phone = request.form['customer_phone']
-    customer_payment = request.form['customer_payment']
-
-    return render_template("/user/mypage/user_info.html",
-                           customer_id=customer_id,
-                           customer_name=customer_name,
-                           customer_account=customer_account,
-                           customer_password=customer_password,
-                           customer_zipcode=customer_zipcode,
-                           customer_address=customer_address,
-                           customer_phone=customer_phone,
-                           customer_payment=customer_payment,
-                           mode=mode)
+# @is_staff_login
+def user_info():
+    customer_account = "customer1@example.com"
+    mst_custemer=read_customer_customer_account(customer_account)
+    print (mst_custemer)
+    return render_template("/user/mypage/user_info.html")
 
 #会員情報変更入力
 @app.route("/user_info_change", methods=["GET", "POST"])
