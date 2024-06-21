7ce54b8c4c3fc136d7171d99c6cb618b98f7031e
NishikawaMisaki <nishikawa.misaki.mu@mynavi.jp>
2024-06-21 02:32:04 +0000

    西川

--- flask_app/views/user/user_ticket.py
+++ flask_app/views/user/user_ticket.py
@@ -3,6 +3,9 @@ from flask_app.__init__ import app
 from flask_app.messages import ErrorMessages, InfoMessages
 from flask_app.models.functions.staff import read_staff_staff_account
 from flask_app.views.staff.common.staff_common import is_staff_login
+from flask_app.database import db
+from flask_app.models.mst_ticket import Mst_ticket
+from flask_app.models.mst_event import Mst_event
 
 #myチケット一覧画面
 @app.route("/my_ticket", methods=["GET", "POST"])
@@ -10,23 +13,26 @@ from flask_app.views.staff.common.staff_common import is_staff_login
 def my_ticket():
     return render_template("user/ticket_manage/my_ticket.html")
 
-
 #myチケット詳細画面
 @app.route("/ticket_info", methods=["GET", "POST"])
 @is_staff_login
 def ticket_detail():
     return render_template("user/ticket_manage/ticket_info.html")
 
-
-#myチケットキャンセル
+#イベントIDとチケットIDを条件に抽出し、myチケットキャンセル画面に表示
 @app.route("/ticket_cancel", methods=["GET", "POST"])
 @is_staff_login
-def ticket_cancel():
-    return render_template("user/ticket_manage/cancel/ticket_cancel.html")
+def read_ticket(event_id,event_category_id):
+    ticket_list = Mst_ticket.query.filter(
+    Mst_ticket.event_id == event_id).all()
+    from flask_app.models.mst_event import Mst_event
+    return render_template("user/ticket_manage/cancel/ticket_cancel.html"),ticket_list,event
 
-
-#myチケットキャンセル完了
+# チケットを削除し、削除完了画面を表示
 @app.route("/ticket_cancel_comp", methods=["GET", "POST"])
 @is_staff_login
-def ticket_cancel_comp():
+def delete_ticket(ticket_id):
+    ticket = Mst_ticket.query.get(ticket_id)
+    db.session.delete(ticket)
+    db.session.commit()
     return render_template("user/ticket_manage/cancel/ticket_cancel_comp.html")
